d3ea172df1b52d735c99443d6d36a662f5e773a0
mdMawl <mmaulanaa299@gmail.com>
2024-06-07 13:01:42 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut debitis dolor quaerat eius neque perferendis ratione reiciendis odit, ea ad sunt cumque dolores laborum enim. Sequi aliquam rerum adipisci vero!</p>
 </body>
 </html>
